286611e28402e6df0a3d81fe1c57ff38646db5cb
sakamuuy <sakamuu.y@gmail.com>
2022-04-18 23:51:26 +0900

    pointer

--- main.go
+++ main.go
@@ -5,7 +5,17 @@ import "fmt"
 func main() {
 	n := 123
 	p := &n
-	fmt.Println("number:", n)
-	fmt.Println("pointer:", p)
-	fmt.Println("value:", *p)
+	q := &p
+	m := 10000
+	p2 := &m
+	q2 := &p2
+	fmt.Printf("p value:%d, address%p\n", *p, p)
+	fmt.Printf("q value:%d, address%p\n", **q, *q)
+	fmt.Printf("p2 value:%d, address%p\n", *p2, p2)
+	fmt.Printf("q2 value:%d, address%p\n", **q2, *q2)
+	pb := p
+	p = p2
+	p2 = pb
+	fmt.Printf("p value:%d, address%p\n", *p, p)
+	fmt.Printf("p2 value:%d, address%p\n", *p2, p2)
 }
