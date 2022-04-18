9b9342385b38cab462734010a5f57c4f5ba9b14c
sakamuuy <sakamuu.y@gmail.com>
2022-04-18 23:54:41 +0900

    reference

--- main.go
+++ main.go
@@ -3,19 +3,36 @@ package main
 import "fmt"
 
 func main() {
+	// n := 123
+	// p := &n
+	// q := &p
+	// m := 10000
+	// p2 := &m
+	// q2 := &p2
+	// fmt.Printf("p value:%d, address%p\n", *p, p)
+	// fmt.Printf("q value:%d, address%p\n", **q, *q)
+	// fmt.Printf("p2 value:%d, address%p\n", *p2, p2)
+	// fmt.Printf("q2 value:%d, address%p\n", **q2, *q2)
+	// pb := p
+	// p = p2
+	// p2 = pb
+	// fmt.Printf("p value:%d, address%p\n", *p, p)
+	// fmt.Printf("p2 value:%d, address%p\n", *p2, p2)
+}
+
+func run() {
 	n := 123
-	p := &n
-	q := &p
-	m := 10000
-	p2 := &m
-	q2 := &p2
-	fmt.Printf("p value:%d, address%p\n", *p, p)
-	fmt.Printf("q value:%d, address%p\n", **q, *q)
-	fmt.Printf("p2 value:%d, address%p\n", *p2, p2)
-	fmt.Printf("q2 value:%d, address%p\n", **q2, *q2)
-	pb := p
-	p = p2
-	p2 = pb
-	fmt.Printf("p value:%d, address%p\n", *p, p)
-	fmt.Printf("p2 value:%d, address%p\n", *p2, p2)
+	fmt.Printf("value:%d.\n", n)
+	change1(n)
+	fmt.Printf("value:%d.\n", n)
+	change2(&n)
+	fmt.Printf("value:%d.\n", n)
+}
+
+func change1(n int) {
+	n *= 2
+}
+
+func change2(n *int) {
+	*n *= 2
 }
